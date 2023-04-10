887f08714c4264119de5024f4d4b3738b0b3aac6
Marcel Gleis <marcel@gleis.de>
2023-04-10 14:59:14 +0200

    bugfix: value was not correctly handed over to next filter action

--- hooks.js
+++ hooks.js
@@ -16,7 +16,7 @@ class Hooks {
         };
 
         isNotInternal && this.execute.call(this, 'hooks_action_register', actionDef);
-        this.hookActionsMap[hookName] = this.hookActionsMap[hookName] || [];
+        this.hookActionsMap.hasOwnProperty(hookName) || (this.hookActionsMap[hookName] = []);
         this.hookActionsMap[hookName].push(actionDef);
     }
 
@@ -26,16 +26,21 @@ class Hooks {
 
         isNotInternal && this.execute.call(this, 'hooks_actions_execute', ...arguments);
 
-        let actions = this.hookActionsMap[hookName] || [];
+        if (!this.hookActionsMap.hasOwnProperty(hookName))
+            return;
+
+        let argsWithoutHookName = Array.from(arguments);
+        argsWithoutHookName.shift();
+
+        let actions = this.hookActionsMap[hookName];
         for (var i = 0; i < actions.length; i++) {
             let actionDef = actions[i];
-
-            let argsWithoutHookName = Array.from(arguments);
-            argsWithoutHookName.shift();
 
             isNotInternal && this.execute.call(this, 'hooks_action_execute', actionDef, argsWithoutHookName);
             result = actionDef.callback.apply(this, argsWithoutHookName);
             isNotInternal && this.execute.call(this, 'hooks_action_executed', actionDef, result, argsWithoutHookName);
+
+            argsWithoutHookName[0] = result;
         }
 
         isNotInternal && this.execute.call(this, 'hooks_actions_executed', ...arguments);
